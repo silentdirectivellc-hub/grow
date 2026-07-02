45948d9df065dbb87e1123c886a00308aeea56ec
silentdirectivellc-hub <silentdirectivellc@gmail.com>
2026-07-01 20:25:53 -0700

    GoonBuster v2: rewire progress, lessons, journal, relapse intel, badges (goonbuster/app/sw.js)

--- goonbuster/app/sw.js
+++ goonbuster/app/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'gb-v1';
+var CACHE = 'gb-v2';
 var ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
