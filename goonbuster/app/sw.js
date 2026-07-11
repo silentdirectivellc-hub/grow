794e1b2886242d692cfc5672523492699dc7ae76
silentdirectivellc-hub <silentdirectivellc@gmail.com>
2026-07-11 00:12:55 -0700

    goonbuster app: update sw.js

--- goonbuster/app/sw.js
+++ goonbuster/app/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'gb-v4';
+var CACHE = 'gb-v5';
 var ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
